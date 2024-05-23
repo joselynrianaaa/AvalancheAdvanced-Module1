bebb2d57f85d2edaac13e6dd6da7dc65bd940e7f
Joselyn Riana <113001778+joselynrianaaa@users.noreply.github.com>
2024-05-23 13:08:16 +0530

    Update Subnet.sol

--- Subnet.sol
+++ Subnet.sol
@@ -6,8 +6,8 @@ contract ERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "Joselyn";
-    string public symbol = "JOSE";
+    string public name = "JoselynRiana";
+    string public symbol = "JO";
     uint8 public decimals = 18;
 
 		event Transfer(address indexed from, address indexed to, uint value);
